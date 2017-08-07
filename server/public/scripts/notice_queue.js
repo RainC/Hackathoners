f8f0d7d97def06503a0722b89a39895b40d021b3
clucle <wjdenwls123@gmail.com>
2017-08-07 23:11:09 +0900

    feat: update queue => work with interval

--- server/public/scripts/notice_queue.js
+++ server/public/scripts/notice_queue.js
@@ -5,8 +5,25 @@ var notices = [
 ];
 
 function enqueue(content, type, time) {
-    var obj = {cotent: content, type: type, time: time};
+    var obj = {content: content, type: type, time: time};
     if (type === 'broadcast') notices.unshift(obj);
     else notices.push(obj)
+    
+    if (notices.length == 1) doTask();
 }
 
+function dequeue() {
+    var obj = {content: notices[0].content, type: notices[0].type, time: notices[0].time}
+    notices.shift();
+    return obj;
+}
+
+function doTask() {
+    // clear
+    if (notices.length == 0) return ;
+    setTimeout("doTask()", 5000);
+    var obj = dequeue();
+    // action
+}
+
+doTask();
