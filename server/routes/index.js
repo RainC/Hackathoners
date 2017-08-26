08e2e32d66c4cef8dc304c74f75b32c08093b94f
clucle <wjdenwls123@gmail.com>
2017-08-27 03:48:38 +0900

    feat: update timer, manage schedule

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// routes carousel in signView
+router.get('/schedule', function(req, res) {
+  res.render('schedule');
+});
+router.get('/rank', function(req, res) {
+  res.render('rank');
+});
+router.get('/information', function(req, res) {
+  res.render('information');
+});
+
 module.exports = router;
